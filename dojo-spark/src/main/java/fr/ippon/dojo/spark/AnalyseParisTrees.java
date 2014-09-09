ef9b4a9bdafb14c7f1ce4740c188d520d2894426
ahars <ahars@ippon.fr>
2014-09-09 15:25:27 +0200

    alexis_branch

--- dojo-spark/src/main/java/fr/ippon/dojo/spark/AnalyseParisTrees.java
+++ dojo-spark/src/main/java/fr/ippon/dojo/spark/AnalyseParisTrees.java
@@ -2,19 +2,29 @@ package fr.ippon.dojo.spark;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class AnalyseParisTrees {
+
     public static void main(String[] args) {
 
-        final String PATH = "/home/dojo/workspace/coding-dojo-spark/";
+        final String PATH = "/Users/seigneurin/dev/coding-dojo-spark/";
 
         SparkConf conf = new SparkConf()
                 .setAppName("paris-arbresalignementparis2010")
                 .setMaster("local");
+                //.setMaster("spark://192.168.1.48:7077")
+                //.setJars(new String[]{PATH + "/dojo-spark/target/dojo-spark-0.0.1-SNAPSHOT.jar"});
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         String filename = PATH + "/data/paris-arbresalignementparis2010/arbresalignementparis2010.csv";
+
         sc.textFile(filename)
-                .count();
+                .filter(s -> s.startsWith("geom_x_y") == false)
+                .map(line -> line.split(";"))
+                .filter(x -> x[3].equals("") == false)
+                .mapToPair(s -> new Tuple2<String, Long>(s[3], 1l))
+                .reduceByKey((x, y) -> x + y)
+                .foreach(x -> System.out.println(x._1 + " = " + x._2));
     }
 }
--- dojo-spark/src/main/java/fr/ippon/dojo/spark/AnalyseWikipediaWorldCup.java
+++ dojo-spark/src/main/java/fr/ippon/dojo/spark/AnalyseWikipediaWorldCup.java
@@ -1,24 +1,52 @@
 package fr.ippon.dojo.spark;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class AnalyseWikipediaWorldCup {
+
     public static void main(String[] args) {
 
-        final String PATH = "/home/dojo/workspace/coding-dojo-spark/";
+        final String PATH = "/Users/seigneurin/dev/coding-dojo-spark/";
 
         SparkConf conf = new SparkConf()
                 .setAppName("wikipedia-pagecounts worldcup")
                 .setMaster("local");
+                //.setMaster("spark://192.168.1.48:7077")
+                //.setJars(new String[]{PATH + "/dojo-spark/target/dojo-spark-0.0.1-SNAPSHOT.jar"});
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         String worldcupPages = PATH + "/data/wikipedia-worldcup-pages/worldcup-pages.txt";
-        sc.textFile(worldcupPages, 10);
-        //...
+        JavaPairRDD<String, String> worldcupPagesRDD = sc.textFile(worldcupPages, 10)
+                .map(line -> line.split("\t"))
+                .mapToPair(s -> new Tuple2<String, String>(s[0] + " " + s[3].toLowerCase(), s[1] + " - " + s[2]));
 
+        // DEBUG
+        if (!true) {
+            worldcupPagesRDD.foreach(s -> System.out.println(s._1));
+        }
         String worldcupViews = PATH + "/data/wikipedia-pagecounts-days/pagecounts-*";
-        sc.textFile(worldcupViews);
-        // ...
+        JavaPairRDD<String, Long> worldcupViewsRDD = sc.textFile(worldcupViews)
+                .map(line -> line.split(" "))
+                .mapToPair(s -> new Tuple2<String, Long>(s[0] + " " + s[1].toLowerCase(), Long.parseLong(s[2])))
+                .reduceByKey((x, y) -> x + y, 10);
+
+        // DEBUG
+        if (!true) {
+            worldcupViewsRDD.foreach(s -> System.out.println(s._1));
+        }
+        JavaPairRDD<String, scala.Tuple2<Long, String>> worldcupPageViewsRDD = worldcupViewsRDD
+                .join(worldcupPagesRDD)
+                .cache();
+        worldcupPageViewsRDD
+                .mapToPair(x -> new Tuple2<Long, String>(x._2._1, x._2._2))
+                .sortByKey()
+                .saveAsTextFile(PATH + "/data/res-worldcup-pageviews-byviews");
+        worldcupPageViewsRDD
+                .mapToPair(x -> new Tuple2<String, Long>(x._2._2, x._2._1))
+                .sortByKey()
+                .saveAsTextFile(PATH + "/data/res-worldcup-pageviews-bycountry");
     }
 }
